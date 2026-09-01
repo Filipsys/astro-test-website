6fc9a8b68957c5095f40f538a4261fd03bc068c2
Filipsys <filipsysak@gmail.com>
2026-09-01 11:57:48 +0200

    mistype fix

--- src/app/fun/page.tsx
+++ src/app/fun/page.tsx
@@ -39,9 +39,8 @@ export default function Fun() {
           style={{
             backgroundImage: "url(/glitch.webp)",
             backgroundSize: "30vw 30vw",
-          }}
+          }}>
           <div className="z-10 bg-zinc-950 max-w-7xl [font-family:serif] flex flex-col h-dvh [box-shadow:_0px_0px_50px_30px_rgba(0,_0,_0,_1)]">
->
             <div className="w-full h-dvh flex flex-row">
               <div className="h-full w-2/3">
                 <div className="w-full flex justify-center ">
